c57c39d2698dd64842bc631e2f5fe7bfb546a0a0
Tyler Dierden <orbitmechanic@protonmail.com>
2019-10-15 21:26:51 -0700

    Created basic Gemini order

--- utopia.js
+++ utopia.js
@@ -1,1 +1,8 @@
-console.log("Welcome to Utopia.")
+const GeminiAPI = require("gemini-api").default;
+const secret = "3uM8TEsyq5JYufCyZSvJKfubWfzZ";
+const key = "account-o00NhmV8zpSaBCq6WBDc";
+const restClient = new GeminiAPI({key, secret, sandbox:true});
+
+// Create a limit order
+restClient.newOrder({amount:10,price:10,side:"buy",symbol:"ethusd"})
+.then(response => console.log(response));
