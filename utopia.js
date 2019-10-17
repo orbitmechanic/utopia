f2e9b05ab4424f215578a1b690ddfa3cdea2c299
marsrvr <marsrvr@gmail.com>
2019-10-16 17:08:24 -0700

    Class example: getAllSymbols()

--- utopia.js
+++ utopia.js
@@ -3,6 +3,5 @@ const secret = "3uM8TEsyq5JYufCyZSvJKfubWfzZ";
 const key = "account-o00NhmV8zpSaBCq6WBDc";
 const restClient = new GeminiAPI({key, secret, sandbox:true});
 
-// Create a limit order
-restClient.newOrder({amount:10,price:10,side:"buy",symbol:"ethusd"})
-.then(response => console.log(response));
+// Get all symbols
+restClient.getAllSymbols().then(response => console.log(response));
