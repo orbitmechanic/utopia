54447596c6e228d3ff83eb1adda7e8d80e869651
Tyler Dierden <orbitmechanic@protonmail.com>
2019-10-20 17:06:42 -0700

    Adding CryptoCompare API

--- utopia.js
+++ utopia.js
@@ -1,8 +1,22 @@
+global.fetch = require("node-fetch");
 const GeminiAPI = require("gemini-api").default;
 const secret = "3uM8TEsyq5JYufCyZSvJKfubWfzZ";
 const key = "account-o00NhmV8zpSaBCq6WBDc";
+const CCAPIKey = "2c24e37ce19890f3eda73ae54fbf99fd60b49ccc136c789cd3f5bfcec67f317c";
 const restClient = new GeminiAPI({key, secret, sandbox:true});
 
-// Create a limit order
+const CryptoCompareAPI = require("cryptocompare");
+CryptoCompareAPI.setApiKey(CCAPIKey);
+
+// Create/cancel a limit order
 restClient.newOrder({amount:10,price:10,side:"buy",symbol:"ethusd"})
-.then(response => console.log(response));
+.then(response => restClient.cancelOrder({order_id:response.order_id}))
+.then(response => console.log(response))
+.catch(error => console.log(error));
+
+
+// Get list of all coins
+CryptoCompareAPI.coinList()
+.then(coinList => {
+    console.log(coinList)
+})
