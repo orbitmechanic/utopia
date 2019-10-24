cab19b17c8e4fa1fcfe477a294488f7a969a2710
marsrvr <marsrvr@gmail.com>
2019-10-24 12:36:15 -0700

    Cleanup

--- utopia.js
+++ utopia.js
@@ -7,15 +7,6 @@ const restClient = new GeminiAPI({key, secret, sandbox:true});
 
 const indicators = require("./indicators.js");
 
-/* Create/cancel a limit order
-restClient.newOrder({amount:10,price:10,side:"buy",symbol:"ethusd"})
-.then(response => restClient.cancelOrder({order_id:response.order_id}))
-.then(response => console.log(response))
-.catch(error => console.log(error)); */
-
-
-
 indicators.movingAverage('ETH','USD', 100,'hour',function(result){
     console.log('MA is: ', result)
 });
-
